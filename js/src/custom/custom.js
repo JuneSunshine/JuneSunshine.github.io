19f9e2622598dabc4d3abc504b016355c42309a7
Jingyang Li <ljygeek@gmail.com>
2017-10-25 22:51:05 -0400

    Site updated: 2017-10-25 22:51:05

---
+++ js/src/custom/custom.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function () {
+  $(window).scroll(function(){
+    $(".top-scroll-bar").attr("style", "width: " + ($(this).scrollTop() / ($(document).height() - $(this).height()) * 100) + "%; display: block;");
+  });
+});
+
